6b26a9ba3d51451d0f322bb2c5a7e7280df95860
Wensheng | Vincent LI <wensheng.li.mit@gmail.com>
2026-04-12 16:52:56 +0930

    add initial files

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req,res) => {
+    res.render("index.ejs");
+});
+
+app.get("/blogs", (req, res) => {
+    res.render("blogs.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
