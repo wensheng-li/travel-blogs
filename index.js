aa46b22fe1a8cc121e4a81ad8f31262f9f8a72f1
Wensheng | Vincent LI <wensheng.li.mit@gmail.com>
2026-04-13 16:38:45 +0930

    update the index.js & package-lock.json to use the dotenv

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-import express from "express";
+// Load environment variables from .env.dev file
+require('dotenv').config({ path: '.env.dev' });
 
+const express = require("express");
 const app = express();
-const port = 3000;
-
 app.set("view engine", "ejs");
+
+// Set envrionment variable to be globally EJS templates
+app.locals.siteName = process.env.SITE_NAME;
+app.locals.env = process.env.NODE_ENV;
+const port = process.env.PORT || 3001;
+
 app.use(express.static("public"));
 
 app.get("/", (req,res) => {
@@ -24,4 +30,5 @@ app.get("/contact", (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    console.log(`Current environment: ${process.env.NODE_ENV}`);
 });
